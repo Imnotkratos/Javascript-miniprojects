d7007d8ad6bb97d40069684e507f7baeabd5a090
Stefano Saldarriaga <148514299+Imnotkratos@users.noreply.github.com>
2024-08-19 12:21:03 -0500

    Update AppPersona.js
    My 1st advance of my mini-project

--- AppPersona.js
+++ AppPersona.js
@@ -4,7 +4,64 @@ class Peronsa{
     this.edad = edad;
     this.dni = dni;
   }
+  
+  //Getters and setters
+  
+  //Nombree
   getNombre(){
-    return this._nombre
+    return this._nombre;
+  }
+  setNombre(new_name){
+     this._nombre = new_name; 
+  }
+
+  //Edad
+  getEdad(){
+    return this._edad;
+  }
+  setEdad(new_edad){
+    this._edad = new_edad;
+  }
+
+  //DNI
+  getDNI(){
+    return this._dni;
+  }
+  setDNI(new_dni){
+    this._dni = new_dni;
   }
 }
+
+class Estudiante extends Persona{
+  constructor(nombre,edad,dni,carrera){
+    super(nombre,edad,dni);
+    this.carrera = carrera;
+    this.FING = ["Ingenieria de Sistemas","Ingenieria Civil","Ingenieria Industrial"];
+    this.FARQ = ["Arquitectura"]
+  }
+  //Getter and setter
+  getCarrera(){
+    return this._carrera;
+  }
+  setCarrera(new_carrera){
+    this._carrera = new_carrera;
+  }
+  verificarFacultad(){
+    for(let i = 0,i < 3, i++){
+     if(this.carrera == this.FING[i]){
+       console.log("Perteneces a la facultad de Ingeniería");
+       break;
+     }
+      else if(this.carrera 00 this.FARQ[i]){
+        console.log("Perteneces a la Facultad de Arquitectura");
+       break;
+      }
+    }
+  }
+  
+  
+
+}
+
+
+
